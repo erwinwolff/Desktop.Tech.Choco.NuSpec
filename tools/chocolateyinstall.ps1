7220075dc86822986c6066e8388208731271ab41
Erwin <erwinwolff@hotmail.com>
2019-02-15 13:48:08 +0100

    Add files via upload

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,21 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://desktoptech.azureedge.net/Setup.exe' 
-$url64      = 'https://desktoptech.azureedge.net/Setup.exe' 
+$url        = 'http://mirror.desktoptech.io/Setup.exe' 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'Desktop.Tech'
 
-  checksum      = ''
+  checksum      = '7BD92537DA15CA09C0CAFB60699621160648F99E88EAD78F592E42188E1181EF'
   checksumType  = 'sha256' 
-  checksum64    = ''
-  checksumType64= 'sha256'
 
   silentArgs    = "--silent" 
   validExitCodes= @(0, 3010, 1641) 
